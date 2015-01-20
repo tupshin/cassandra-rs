b6d7566a7032cc02f86f37c7575d97a44e0ea24a
Tupshin Harper <tupshin@tupshin.com>
2015-01-19 23:04:06 -0500

    added documentation

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['cql_ffi'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='cql_ffi/enum.CassError.html' title='cql_ffi::CassError'>CassError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='cql_ffi/enum.CassError.html' title='cql_ffi::CassError'>CassError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- src/examples/uuids.rs
+++
@@ -1,3 +0,0 @@
-pub fn main() {
-	panic!("not implemented yet");
-}

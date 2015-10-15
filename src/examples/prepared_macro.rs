ee6e0207c4bc731bab2b2f8e17066ee8296d8c7d
Jon Haddad <jon@jonhaddad.com>
2015-10-15 16:39:28 -0700

    adding prepared statement macro

---
+++ src/examples/prepared_macro.rs
@@ -0,0 +1,97 @@
+extern crate cassandra;
+
+use cassandra::*;
+
+static CREATE_KEYSPACE:&'static str = "CREATE KEYSPACE examples WITH replication = { \'class\': \
+                                       \'SimpleStrategy\', \'replication_factor\': \'3\' };";
+static CREATE_TABLE:&'static str = "CREATE TABLE examples.basic (key text, bln boolean, flt float, \
+                                    dbl double, i32 int, i64 bigint, PRIMARY KEY (key));";
+static INSERT_QUERY:&'static str = "INSERT INTO examples.basic (key, bln, flt, dbl, i32, i64) \
+                                    VALUES (?, ?, ?, ?, ?, ?);";
+static SELECT_QUERY:&'static str = "SELECT * FROM examples.basic WHERE key = ?";
+
+#[derive(Debug,PartialEq)]
+struct Basic {
+    bln: bool,
+    flt: f32,
+    dbl: f64,
+    i32: i32,
+    i64: i64,
+}
+
+fn insert_into_basic(session: &mut Session,
+                     key: &str,
+                     basic: &mut Basic)
+                     -> Result<(), CassandraError> {
+    println!("Creating statement");
+    let mut statement = Statement::new(INSERT_QUERY, 6);
+    statement
+        .bind_string(0, key).unwrap()
+        .bind_bool(1, basic.bln).unwrap()
+        .bind_float(2, basic.flt).unwrap()
+        .bind_double(3, basic.dbl).unwrap()
+        .bind_int32(4, basic.i32).unwrap()
+        .bind_int64(5, basic.i64).unwrap();
+    println!("Executing insert statement");
+    try!(session.execute_statement(&statement).wait());
+    println!("Insert execute OK");
+    Ok(())
+}
+
+
+fn select_from_basic(session: &mut Session,
+                     prepared: &PreparedStatement,
+                     key: &str,
+                     basic: &mut Basic)
+                     -> Result<(), CassandraError> {
+    let mut statement = prepared.bind();
+    try!(statement.bind_string(0, key));
+    let mut future = session.execute_statement(&statement);
+    match future.wait() {
+        Ok(result) => {
+            println!("{:?}", result);
+            for row in result.iter() {
+                basic.bln = try!(try!(row.get_column(1)).get_bool());
+                basic.dbl = try!(try!(row.get_column(2)).get_double());
+                basic.flt = try!(try!(row.get_column(3)).get_float());
+                basic.i32 = try!(try!(row.get_column(4)).get_int32());
+                basic.i64 = try!(try!(row.get_column(5)).get_int64());
+            }
+            Ok(())
+        }
+        Err(err) => panic!("{:?}",err),
+    }
+}
+
+fn main() {
+    let mut cluster = Cluster::new();
+    cluster.set_contact_points("127.0.0.1").unwrap();
+    cluster.set_protocol_version(3).unwrap();
+    println!("Proto set");
+    let mut session = cluster.connect().unwrap();
+
+    println!("Connected");
+    let mut input = Basic { bln: true, flt: 0.001f32, dbl: 0.0002f64, i32: 1, i64: 2 };
+    let mut output = Basic { bln: false, flt: 0f32, dbl: 0f64, i32: 0, i64: 0 };
+    println!("Executing create keyspace");
+    session.execute(CREATE_KEYSPACE,0);
+    println!("Creating table");
+    session.execute(CREATE_TABLE,0);
+
+    println!("Basic insertions");
+    insert_into_basic(&mut session, "prepared_test", &mut input).unwrap();
+    println!("Preparing");
+
+    let prepared = prepare!("SELECT * FROM examples.basic WHERE key = ?").unwrap();
+
+    // match session.prepare(SELECT_QUERY).unwrap().wait() {
+    //     Ok(prepared) => {
+    //         select_from_basic(&mut session, &prepared, "prepared_test", &mut output).unwrap();
+    //         println!("input: {:?}\nouput: {:?}", input,output);
+    //         assert_eq!(input,output);
+    //     }
+    //     Err(err) => panic!(err),
+    // }
+    let close_future = session.close();
+    close_future.wait().unwrap();
+}
